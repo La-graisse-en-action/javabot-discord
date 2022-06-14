36eb265850fd7b427edffdba51003c7061c27dd4
mrLuisFer <mrluisfeer@gmail.com>
2022-06-13 23:11:29 -0500

    fix setConfig method to return JDA class and add eventListener in the Bot class

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -2,11 +2,16 @@ import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import utils.BotBuilder;
+import utils.Commands;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class DiscordBot extends ListenerAdapter {
+	
 	public static void main(String[] args) throws LoginException, InterruptedException {
 		JDABuilder botBuilder = JDABuilder.createDefault(BotBuilder.getToken());
-		BotBuilder.setConfig(botBuilder);
+		JDA jda = BotBuilder.setConfig(botBuilder);
+		
+		jda.addEventListener(new Commands());
 	}
 }
--- src/main/java/utils/BotBuilder.java
+++ src/main/java/utils/BotBuilder.java
@@ -3,13 +3,19 @@ package utils;
 import javax.security.auth.login.LoginException;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 
 public class BotBuilder {
-	public static void setConfig(JDABuilder builder) throws LoginException {
+	public String prefix = "f-";
+
+	public static JDA setConfig(JDABuilder builder) throws LoginException {
 		builder.setActivity(Activity.watching("Hentai"));
-		builder.build();
+		builder.setStatus(OnlineStatus.ONLINE);
+		JDA jda = builder.build();
+		return jda;
 	}
 
 	public static String getToken() {
