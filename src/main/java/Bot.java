872eda17c54be5e32175c0dc552c140cb15866ff
mrLuisFer <mrluisfeer@gmail.com>
2022-06-13 20:24:57 -0500

    first commit

---
+++ src/main/java/Bot.java
@@ -0,0 +1,9 @@
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class Bot extends ListenerAdapter {
+	public static void main(String[] args) throws LoginException, InterruptedException {
+		// Main Bot Code Here
+	}
+}
---
+++ src/main/java/Listener.java
@@ -0,0 +1,21 @@
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
+
+public class Listener implements EventListener {
+	public static void main(String[] args) throws LoginException, InterruptedException {
+		JDA jda = JDABuilder.createDefault("token").addEventListeners(new Listener()).build();
+	
+		jda.awaitReady();
+	}
+
+	public void onEvent(GenericEvent event) {
+		if(event instanceof ReadyEvent) {
+			System.out.println("Api is ready!");
+		}
+	}
+}
