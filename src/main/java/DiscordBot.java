5569996ad0e9fa054c80961c800aa33d1be9a4b7
mrLuisFer <mrluisfeer@gmail.com>
2022-06-13 21:38:50 -0500

    use getToken method for the JDABuilder

--- src/main/java/DiscordBot.java
+++ src/main/java/DiscordBot.java
@@ -1,14 +1,12 @@
 import javax.security.auth.login.LoginException;
 
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import utils.BotBuilder;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
 
 public class DiscordBot extends ListenerAdapter {
 	public static void main(String[] args) throws LoginException, InterruptedException {
-		JDA botBuilder = JDABuilder.createDefault("token here")
-				.setActivity(Activity.watching("Hentai")).build();
-		
+		JDABuilder botBuilder = JDABuilder.createDefault(BotBuilder.getToken());
+		BotBuilder.setConfig(botBuilder);
 	}
 }
--- src/main/java/Listener.java
+++ src/main/java/Listener.java
@@ -9,12 +9,12 @@ import net.dv8tion.jda.api.hooks.EventListener;
 public class Listener implements EventListener {
 	public static void main(String[] args) throws LoginException, InterruptedException {
 		JDA jda = JDABuilder.createDefault("token").addEventListeners(new Listener()).build();
-	
+
 		jda.awaitReady();
 	}
 
 	public void onEvent(GenericEvent event) {
-		if(event instanceof ReadyEvent) {
+		if (event instanceof ReadyEvent) {
 			System.out.println("Api is ready!");
 		}
 	}
